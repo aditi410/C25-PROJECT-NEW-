76d9f01d82b9f010c5de7a76e6be8934ee31c6ca
aditi410 <80102162+aditi410@users.noreply.github.com>
2021-05-30 04:36:33 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,37 @@
+class paper{
+    constructor(x,y,r) {
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:1.2,
+            
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.image=loadImage("paper.png");
+        this.body=Bodies.circle(this.x,this.y,(this.r=20)/2,options);
+        World.add(world,this.body);
+    }
+
+
+
+
+
+
+
+    
+  display(){
+  var paperpos=this.body.position;
+  push()
+  translate(paperpos.x,paperpos.y);
+  rectMode(CENTRE);
+  fill(225,0,225);
+   Pop();
+
+
+  }
+
+
+
+}
